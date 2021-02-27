1cb54c5e994456128a77b18a52d47f4b487e34f2
LempekPL <Superlempek2004@gmail.com>
2021-02-27 22:49:49 +0100

    ą

--- client/events/bot/message.js
+++ client/events/bot/message.js
@@ -18,10 +18,10 @@ module.exports = async (message, client) => {
         message.reply("My prefix is `" + prefix + "`");
     }
     if (!message.content.startsWith(prefix)) return;
-    
+
     //bot global bans
     let gbans = await db.get("bot", client.user.id, "globalBans");
-    if (gbans.length>0) {
+    if (gbans.length > 0) {
         if (gbans.includes(message.author.id)) {
             await require("../../../util/util").gban(message, client);
             return;
@@ -48,16 +48,11 @@ async function runcmd(command, commandfile, args, message, client) {
             message.delete();
             message.reply(`you need to wait \`${coolmins}\`m \`${coolsecs}\`s between commands.`).then(message => {
                 message.delete({
-                    timeout: slowmode*1000,
+                    timeout: slowmode * 1000,
                     reason: 'Autoremove'
                 });
             });
         } else {
-            // doesn't give cooldown for server administrators and main owner
-            if (!message.member.hasPermission("ADMINISTRATOR") || message.author.id != config.settings.ownerid) {
-                cooldown.add(message.author.id);
-            }
-
             // running command
             await commandfile.run(client, message, args);
 
@@ -73,24 +68,32 @@ async function runcmd(command, commandfile, args, message, client) {
             await db.update("bot", client.user.id, "commands", comCount);
             await db.update("users", message.author.id, "favCommands", userCommand);
 
-            // logging used command
-            let whook = new Discord.WebhookClient(config.webhooks.all.split("/")[5], config.webhooks.all.split("/")[6]);
-            let loggen = new Discord.MessageEmbed;
-            loggen.setTitle(message.author.tag + " used command: " + command);
-            loggen.setDescription(message.content);
-            loggen.setColor('#ffff00')
-            loggen.setFooter("© Lempek", client.users.cache.get('249253855613812736').avatarURL);
-            loggen.addField("Server ID", message.guild.name + " (" + message.guild.id + ")");
-            loggen.addField("Channel ID", message.channel.name + " (" + message.channel.id + ")");
-            loggen.addField("Message ID", message.id);
-            loggen.addField("Message Created", message.createdAt);
-            loggen.addField("User Message Link", "https://discordapp.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
-            loggen.addField("Command", comCount);
-            loggen.addField("Owner", message.author.tag + " (" + message.author.id + ")");
-            loggen.setTimestamp();
-            if (message.author.id != config.settings.ownerid) {
+
+            // ignoring owners
+            if (message.author.id != config.settings.ownerid || !config.settings.subowners.includes(message.author.id)) {
+                // ingnore cooldown for server administrators
+                if (!message.member.hasPermission("ADMINISTRATOR")) {
+                    cooldown.add(message.author.id);
+                }
+                // logging used command
+                let whook = new Discord.WebhookClient(config.webhooks.all.split("/")[5], config.webhooks.all.split("/")[6]);
+                let loggen = new Discord.MessageEmbed;
+                loggen.setTitle(message.author.tag + " used command: " + command);
+                loggen.setDescription(message.content);
+                loggen.setColor('#ffff00')
+                loggen.setFooter("© Lempek", client.users.cache.get('249253855613812736').avatarURL);
+                loggen.addField("Server ID", message.guild.name + " (" + message.guild.id + ")");
+                loggen.addField("Channel ID", message.channel.name + " (" + message.channel.id + ")");
+                loggen.addField("Message ID", message.id);
+                loggen.addField("Message Created", message.createdAt);
+                loggen.addField("User Message Link", "https://discordapp.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
+                loggen.addField("Command", comCount);
+                loggen.addField("Message Owner", message.author.tag + " (" + message.author.id + ")");
+                loggen.setTimestamp();
                 await whook.send(loggen);
             }
+
+            // removing cooldown
             setTimeout(() => {
                 cooldown.delete(message.author.id)
             }, slowmode * 1000)
