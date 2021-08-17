634fe5e34fd23580556e5c3e9b4eb83aa463129a
LempekPL <Superlempek2004@gmail.com>
2021-08-17 20:03:26 +0200

    cleaning

--- client/commands/imageManipulation/blurple.js
+++ client/commands/imageManipulation/blurple.js
@@ -4,7 +4,7 @@ module.exports.info = {
     name: "blurple",
     tags: ["api","badosz","picture","fun"]
 }
-
+// TODO: better image check
 module.exports.run = async (client, message, args) => {
     await client.util.obraziumHandler(client, message, module.exports.info.name, "buffer", args[0]?.startsWith("https://") ? "urlImage" : "urlAvatar", args[0]);
 }
--- client/commands/imageManipulation/colorify.js
+++ client/commands/imageManipulation/colorify.js
@@ -4,7 +4,7 @@ module.exports.info = {
     name: "colorify",
     tags: ["api","badosz","picture","fun"]
 }
-
+// TODO: if (!(/^(#+[a-fA-F0-9]{6}|#+[a-fA-F0-9]{3})$/.test(args[0]))) return client.emit("uisae", "U98", message, "");
 module.exports.run = async (client, message, args) => {
     await client.util.obraziumHandler(client, message, module.exports.info.name, "buffer", args[1]?.startsWith("https://") ? "urlImage+hex" : "urlAvatar+hex", args);
 }
